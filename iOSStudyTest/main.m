e43025edb12f43a18841dd11db2b1797178a975a
Hugo <lipeng@qiniu.com>
2019-09-30 23:15:24 +0800

    NSMutalbeString

--- iOSStudyTest/main.m
+++ iOSStudyTest/main.m
@@ -223,6 +223,43 @@ int main(int argc, char * argv[]) {
         unichar c = [string24 characterAtIndex:3];
         
         
+        
+        
+        //NSMutableString中的内容是可以修改的
+        //NSMutableString是继承NSString类的
+        //1.--------------------创建字符串
+        //和NSString的创建方法相同
+        NSMutableString *string25 = [[NSMutableString alloc] initWithString:@"hello"];
+        
+        //但是不能使用以下方式创建
+        //因为以下方式创建的字符串是不可变的，其值是放到常量池中的，不可变得
+        //NSMutableString *string1 = @"hello";
+        
+        
+        //2.---------------------insertString
+        //在源字符串中指定的index中插入字符串，不会产生新的对象
+        [string25 insertString:@"可变" atIndex:0];
+        
+        
+        //3.---------------------appendString
+        //追加字符串,不会产生新的对象
+        [string25 appendString:@"可变"];
+        
+        
+        //4.---------------------deleteCharactersInRange
+        //删除字符串中指定范围的字串内容
+        NSMutableString *string26 = [NSMutableString stringWithString:@"hello"];
+        NSRange range1 = {1,3};
+        [string26 deleteCharactersInRange:range1];
+        
+        
+        //5.---------------------replaceCharactersInRange
+        //替换字符串内容
+        NSMutableString *string27 = [NSMutableString stringWithString:@"hello"];
+        NSRange ranges = [string27 rangeOfString:@"ll"];//先查找出需要替换字符串的范围
+        [string27 replaceCharactersInRange:ranges withString:@"ee"];
+        
+        
     }
     return 0;
 }
